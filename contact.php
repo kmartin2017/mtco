5e7202a07ad6c73b8c19593a4267b1452bc6f1cf
Kathleen Martin <kathleenmartin@Kathleens-MacBook-Pro.local>
2020-06-07 21:30:39 -0400

    haha

--- contact.php
+++ contact.php
@@ -1,37 +1,24 @@
 <?php
 
-if (isset($_POST['submit'])){
-	$field_name = $_POST['name'];
-	$field_email = $_POST['email'];
-	$field_phone = $_POST['phone'];
-	$field_message = $_POST['message'];
-}
+
+$field_name = $_POST['name'];
+$field_email = $_POST['email'];
+$field_phone = $_POST['phone'];
+$field_message = $_POST['message'];
+
 
 $mail_to = 'management@themtco.com';
-$subject = 'New Message from theMTCo website! ->'.$field_name;
+$subject = 'New Message from theMTCo website!';
 
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Phone: '.$field_phone."\n";
-$body_message .= 'Message: '.$field_message;
+// $body_message = 'From: '.$field_name."\n";
+// $body_message .= 'E-mail: '.$field_email."\n";
+// $body_message .= 'Phone: '.$field_phone."\n";
+// $body_message .= 'Message: '.$field_message;
 
-$headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
+$body_message = $field_message;
 
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
+$headers = 'From: $field_email \r\n';
 
-if ($mail_status) { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Sucess Message');
-		window.location = 'contact.html';
-	</script>
-<?php
-}
-else { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Error Message');
-		window.location = 'contact.html';
-	</script>
-<?php
-}
+$mail_status = mail($mail_to, $subject, $body_message, $headers) or die("Error Message");
+echo "Thank You!" . " -" . "<a href='index.html'> Return Home</a>";
 ?>
