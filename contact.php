f7c22a28bf29aee6a14d346a278eb9b819f7cd21
Kathleen Martin <kathleenmartin@Kathleens-MacBook-Pro.local>
2020-06-08 23:37:11 -0400

    redict and sucess message

--- contact.php
+++ contact.php
@@ -8,6 +8,7 @@ $field_message = $_POST['message'];
 
 
 $mail_to = 'management@themtco.com';
+$mail_from = $field_email;
 $subject = 'New Message from theMTCo website!';
 
 // $body_message = 'From: '.$field_name."\n";
@@ -21,7 +22,7 @@ $headers = 'From: $field_email \r\n';
 
 $myfile = fopen("printing_messages.txt", "a+") or die("Unable to open file!");
 
-fwrite($myfile, $mail_to);
+fwrite($myfile, $mail_from);
 fwrite($myfile, "\r\n");
 fwrite($myfile, $subject);
 fwrite($myfile, "\r\n");
@@ -31,8 +32,11 @@ fwrite($myfile, $mail_to);
 fwrite($myfile, "\r\n");
 fwrite($myfile, "\r\n");
 
+alert('Message Sent')
+window.location = 'contact.html'
 
-echo "Thank You!" . " -" . "<a href='index.html'> Return Home</a>";
+
+// echo "Thank You!" . " -" . "<a href='index.html'> Return Home</a>";
 // $mail_status = mail($mail_to, $subject, $body_message, $headers) or die("Error Message");
 // echo "Thank You!" . " -" . "<a href='index.html'> Return Home</a>";
 ?>
